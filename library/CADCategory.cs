f640403912ef7476dc7c6c5f2332bce75b218bee
aep49-ua <aep49@alu.ua.es>
2026-03-25 19:38:46 +0100

    55177138T Creado CADCategory.cs

---
+++ library/CADCategory.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    internal class CADCategory
+    {
+        private string conex;
+
+        public CADCategory()
+        {
+
+            conex = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\Database.mdf;Integrated Security=True";
+
+        }
+
+        public bool Read(ENCategory en)
+        {
+            SqlConnection conexion = new SqlConnection(conex);
+            try
+            {
+                conexion.Open();
+
+                SqlCommand com = new SqlCommand("SELECT * FROM Categories WHERE id=@id", conexion);
+                com.Parameters.AddWithValue("@id", en.id);
+
+                SqlDataReader dat = com.ExecuteReader();
+
+                if (dat.Read())
+                {
+                    en.id = int.Parse(dat["id"].ToString());
+                    en.name = dat["name"].ToString();
+
+                    dat.Close();
+                    return true;
+                }
+
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+        public List<ENCategory> ReadAll()
+        {
+            SqlConnection conexion = new SqlConnection(conex);
+            List<ENCategory> listaCategorias = new List<ENCategory>();
+
+            try
+            {
+                conexion.Open();
+
+                SqlCommand com = new SqlCommand("SELECT * FROM Categories", conexion);
+                SqlDataReader dat = com.ExecuteReader();
+
+                while (dat.Read())
+                {
+                    ENCategory cat = new ENCategory();
+                    cat.id = int.Parse(dat["id"].ToString());
+                    cat.name = dat["name"].ToString();
+
+                    listaCategorias.Add(cat);
+                }
+
+                dat.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                conexion.Close();
+                
+            }
+            
+            return listaCategorias;
+        }
+    }
+}
